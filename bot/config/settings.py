3d950b773cbecdbf51afd805dff3b414743ff8a5
fonkeness <fonkeness@gmail.com>
2025-12-16 03:58:09 +0800

    Settings changes

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,3 +1,11 @@
-TOKEN = "8190550781:AAENWx2f93LcoC61wNcBpbBysTybXReXqoc"
-PASSWORD = "800815"
-ADMIN_ID = 732073473
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+PASSWORD = os.getenv("PASSWORD")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+if not TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set")
